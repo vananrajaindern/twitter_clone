bae5173275d9a0f898a006c6e114e216c5b5b222
ssoonsl <ssoonsl616+gh@gmail.com>
2017-06-02 12:12:27 +0800

    Add Tweet controller tests for new and create

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -13,4 +13,47 @@ RSpec.describe TweetsController, type: :controller do
 
   end
 
+  describe 'GET #new' do
+
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+
+      get :new
+
+    end
+
+    it { expect(assigns(:tweet)).to be_a_new_record }
+
+  end
+
+  describe 'POST #create' do
+
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+
+      post :create, params: { tweet: params }
+    end
+
+    context 'when tweet#save passes' do
+
+      let(:params) { attributes_for(:tweet) }
+
+      it { expect(response).to redirect_to tweets_path }
+
+    end
+
+    context 'when tweet#save fails' do
+
+      let(:params) { attributes_for(:tweet, :invalid) }
+
+      it { expect(response).to render_template(:new) }
+
+    end
+
+  end
+
 end
