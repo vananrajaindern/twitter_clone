d98436a9b2293e9019fa6f99023b3ce80a90e1e7
ssoonsl <ssoonsl616+gh@gmail.com>
2017-06-13 11:43:02 +0800

    Improved followings controller to use build instead of create

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,10 +3,10 @@ class FollowingsController < ApplicationController
   def create
     @follower = current_user
     @followee = User.find(params[:id])
-
     if @followee != @follower
-      Following.create(follower: @follower, followee: @followee)
-    end
+      @following = @follower.is_following.build(followee: @followee)
+      @following.save
+      end
 
     respond_to do |format|
       format.html { redirect_to profile_path(@followee) }
